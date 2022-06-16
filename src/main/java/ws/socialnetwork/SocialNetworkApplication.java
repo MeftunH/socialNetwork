3c9b62317af7cd5be183323dbbca26aaecace7ef
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-16 06:45:10 +0300

    exclude spring security for development

--- src/main/java/ws/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/ws/socialnetwork/SocialNetworkApplication.java
@@ -2,8 +2,11 @@ package ws.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+import java.security.Security;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SocialNetworkApplication {
 
     public static void main(String[] args) {
