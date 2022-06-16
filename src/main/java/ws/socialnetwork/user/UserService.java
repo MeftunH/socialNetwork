f1c11a53983f9c5facca8c4af6ecfea264cb6fe2
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-16 06:43:10 +0300

    decode user's password

--- src/main/java/ws/socialnetwork/user/UserService.java
+++ src/main/java/ws/socialnetwork/user/UserService.java
@@ -1,16 +1,24 @@
 package ws.socialnetwork.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 public class UserService{
     UserRepository userRepository;
+
+    PasswordEncoder passwordEncoder;
+
     //if you have only one constructor and this constructor has values, it is not meaningful to use @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void save(Users user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
         userRepository.save(user);
     }
 }
--- src/main/java/ws/socialnetwork/user/Users.java
+++ src/main/java/ws/socialnetwork/user/Users.java
@@ -1,10 +1,13 @@
 package ws.socialnetwork.user;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
 @Entity
 public class Users {
     @Id
