d409c5141c60db31b7a62db726e3e3ab250a7ce5
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-16 07:12:55 +0300

    add missed service annotation to user service

--- src/main/java/ws/socialnetwork/user/UserService.java
+++ src/main/java/ws/socialnetwork/user/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Service
 public class UserService{
     UserRepository userRepository;
 
