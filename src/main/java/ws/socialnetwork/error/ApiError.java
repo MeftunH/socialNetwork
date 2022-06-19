85f2d691f4416090d9149de27a8ba03328547582
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-20 00:00:32 +0300

    set error content

--- src/main/java/ws/socialnetwork/error/ApiError.java
+++ src/main/java/ws/socialnetwork/error/ApiError.java
@@ -1,4 +1,10 @@
 package ws.socialnetwork.error;
 
 public class ApiError {
+    private String message;
+    private String path;
+    private String timestamp;
+    private int status;
+    private String error;
+    private String exception;
 }
