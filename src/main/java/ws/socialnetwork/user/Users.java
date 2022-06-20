3e32a341bced5269a7ffc785a882b065815c210d
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-21 02:36:45 +0300

    add notnull translation keys

--- src/main/java/ws/socialnetwork/user/Users.java
+++ src/main/java/ws/socialnetwork/user/Users.java
@@ -18,18 +18,18 @@ public class Users {
     private long id;
 
     @Column(name="`USERNAME`")
-    @NotNull
+    @NotNull(message = "{app.constraint.user.username.notNull.Message}")
     @Size(min = 3, max = 20)
     @UniqueUsername
     private String username;
 
-    @NotNull
+    @NotNull(message = "{app.constraint.user.password.notNull.Message}")
     @Column(name="`PASSWORD`")
     @Size(min = 6, max = 80)
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{6,}$", message = "Password must contain at least 6 characters, including UPPER/lowercase, numbers and special characters.")
     private String password;
 
-    @NotNull
+    @NotNull(message = "{app.constraint.user.displayName.notNull.Message}")
     @Size(min = 3, max = 20)
     @Column(name="`DISPLAY_NAME`")
     private String displayName;
