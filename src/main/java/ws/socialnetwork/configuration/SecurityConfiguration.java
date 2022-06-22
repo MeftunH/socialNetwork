f83fc6a576eeda6f8fff4b70a2c480ac2a9e2e31
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-23 00:55:59 +0300

    add auth entry point for disable browser auto behavior

--- src/main/java/ws/socialnetwork/configuration/SecurityConfiguration.java
+++ src/main/java/ws/socialnetwork/configuration/SecurityConfiguration.java
@@ -1,9 +1,17 @@
 package ws.socialnetwork.configuration;
 
 import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
@@ -11,7 +19,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-        http.httpBasic();
+        http.httpBasic().authenticationEntryPoint(new AuthenticationEntryPoint() {
+            @Override
+            public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+                response.sendError(HttpStatus.UNAUTHORIZED.value(),HttpStatus.UNAUTHORIZED.getReasonPhrase());
+            }
+        });
 
         http.
                 authorizeHttpRequests().antMatchers(HttpMethod.POST,"/api/1.0/auth").authenticated()
