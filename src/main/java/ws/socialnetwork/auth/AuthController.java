79ae3ae5e94a86860546e06c233907543f56764b
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-22 23:50:28 +0300

    add logger to auth function

--- src/main/java/ws/socialnetwork/auth/AuthController.java
+++ src/main/java/ws/socialnetwork/auth/AuthController.java
@@ -1,5 +1,6 @@
 package ws.socialnetwork.auth;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,15 +8,19 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import ws.socialnetwork.error.ApiError;
 
+import java.util.logging.Logger;
+
 @RestController
 public class AuthController {
 
+    private static final Logger log = (Logger) LoggerFactory.getLogger(AuthController.class);
     @PostMapping("/api/1.0/auth/")
     ResponseEntity<?> handleAuthentication(@RequestHeader(name = "Authorization",required = false) String authorization) {
     if (authorization == null) {
       ApiError error = new ApiError(401, "Unauthorized request", "/api/1.0/auth");
       return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
     }
+    log.info("Authorization: " + authorization);
     return ResponseEntity.ok().build();
     }
 }
