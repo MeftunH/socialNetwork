fe001e52828b708863724ce4b88f4106c7ef296c
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-22 23:51:41 +0300

    add jackson library non null annotation to api error class

--- src/main/java/ws/socialnetwork/error/ApiError.java
+++ src/main/java/ws/socialnetwork/error/ApiError.java
@@ -1,11 +1,13 @@
 package ws.socialnetwork.error;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 import java.util.Date;
 import java.util.Map;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ApiError {
     private String message;
     private String path;
