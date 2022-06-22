af9d1d8cf0e6cd5e7595dc304ab596cc9088ec65
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-23 02:34:33 +0300

    adapt user auth service

--- src/main/java/ws/socialnetwork/configuration/SecurityConfiguration.java
+++ src/main/java/ws/socialnetwork/configuration/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package ws.socialnetwork.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +18,8 @@ import java.io.IOException;
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    UserAuthService userDetailsService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -30,5 +34,11 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 authorizeHttpRequests().antMatchers(HttpMethod.POST,"/api/1.0/auth").authenticated()
                 .and()
                 .authorizeHttpRequests().anyRequest().permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+
     }
 }
