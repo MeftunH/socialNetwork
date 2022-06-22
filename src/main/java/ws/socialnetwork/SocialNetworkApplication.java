8cd8604be87541a06bad9a7bd2117282a0d0ff10
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-22 17:46:36 +0300

    add user when project init with lambda function

--- src/main/java/ws/socialnetwork/SocialNetworkApplication.java
+++ src/main/java/ws/socialnetwork/SocialNetworkApplication.java
@@ -1,8 +1,13 @@
 package ws.socialnetwork;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import ws.socialnetwork.user.UserRepository;
+import ws.socialnetwork.user.UserService;
+import ws.socialnetwork.user.Users;
 
 import java.security.Security;
 
@@ -12,5 +17,14 @@ public class SocialNetworkApplication {
     public static void main(String[] args) {
         SpringApplication.run(SocialNetworkApplication.class, args);
     }
-
+    @Bean
+    CommandLineRunner createInitialUsers(UserService userService) {
+        return (args) -> {
+            Users user = new Users();
+            user.setUsername("user1");
+            user.setDisplayName("user1_dn");
+            user.setPassword("P4ssword*");
+            userService.save(user);
+        };
+    }
 }
