32ec49c1adbda2f2802b5215cc12a9c03f5203bf
Maftun Hashimli <maftunhashimli@gmail.com>
2022-06-23 05:53:29 +0300

    inject current user annotation to auth controller

--- src/main/java/ws/socialnetwork/auth/AuthController.java
+++ src/main/java/ws/socialnetwork/auth/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import ws.socialnetwork.shared.CurrentUser;
 import ws.socialnetwork.shared.Views;
 import ws.socialnetwork.user.UserRepository;
 import ws.socialnetwork.user.Users;
@@ -19,8 +20,7 @@ public class AuthController {
 
     @PostMapping("/api/1.0/auth")
     @JsonView(Views.BaseView.class)
-    ResponseEntity<?> handleAuthentication(Authentication authentication) {
-    Users user = (Users) authentication.getPrincipal();
+    ResponseEntity<?> handleAuthentication(@CurrentUser Users user) {
     return ResponseEntity.ok(user);
     }
 }
